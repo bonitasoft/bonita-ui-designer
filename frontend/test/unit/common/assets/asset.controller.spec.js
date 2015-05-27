779b38da267fb29c5e2dfa703f459963b0c38e7e
Guillaume EHRET <gui.ehret@gmail.com>
2015-05-27 13:11:01 +0200

    Calls backend to load assets used in a page
    Fix TU

--- frontend/test/unit/common/assets/asset.controller.spec.js
+++ frontend/test/unit/common/assets/asset.controller.spec.js
@@ -10,57 +10,103 @@
       $scope = $injector.get('$rootScope').$new();
       $q = $injector.get('$q');
       assets = $injector.get('assets');
-
-      $injector.get('$controller')('AssetCtrl', {
-        $scope: $scope,
-        artifact: {},
-        artifactRepo: {},
-        mode : 'page',
-        assets: assets
-      });
-
     }));
 
-    it('should put artifact in $scope', function(){
-      expect($scope.component).toEqual({});
-    });
+    describe('Page editor', function(){
 
-    describe('filter on assets', function(){
+      beforeEach(inject(function($injector) {
+        $injector.get('$controller')('AssetCtrl', {
+          $scope: $scope,
+          artifact: {},
+          artifactRepo: {
+            loadAssets : function() {
+              return $q.when({
+                data: [
+                  {name: 'myAsset'}
+                ]
+              });
+            }
+          },
+          mode : 'page',
+          assets: assets
+        });
+      }));
 
-      it('should do nothing by default', function(){
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeTruthy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeTruthy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeTruthy();
+      it('should put assets in $scope', function(){
+        $scope.$digest();
+        expect($scope.assets).toEqual([{name: 'myAsset'}]);
       });
 
-      it('should return false when asset type undefined', function(){
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": undefined })).toBeFalsy();
+      it('should return isAssetPage=true when artifact is a page', function(){
+        expect($scope.isAssetPage).toBeTruthy();
       });
 
-      it('should skip other types if I want a filter only on js', function(){
-        $scope.searchedAsset = $scope.searchedAsset.map(function(elt){
-          if(elt.key!=='js'){
+      /**
+       * Filtering is not available in widget editor
+       */
+      describe('filter on assets', function(){
+
+        it('should do nothing by default', function(){
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeTruthy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeTruthy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeTruthy();
+        });
+
+        it('should return false when asset type undefined', function(){
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": undefined })).toBeFalsy();
+        });
+
+        it('should skip other types if I want a filter only on js', function(){
+          $scope.searchedAsset = $scope.searchedAsset.map(function(elt){
+            if(elt.key!=='js'){
+              elt.filter=false;
+            }
+            return elt;
+          });
+
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeTruthy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeFalsy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeFalsy();
+        });
+
+        it('should skip all types if filter are all actives ', function(){
+          $scope.searchedAsset = $scope.searchedAsset.map(function(elt){
             elt.filter=false;
-          }
-          return elt;
+            return elt;
+          });
+
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeFalsy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeFalsy();
+          expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeFalsy();
         });
-
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeTruthy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeFalsy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeFalsy();
-      });
-
-      it('should skip all types if filter are all actives ', function(){
-        $scope.searchedAsset = $scope.searchedAsset.map(function(elt){
-          elt.filter=false;
-          return elt;
-        });
-
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.js", "type": "js" })).toBeFalsy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.css", "type": "css" })).toBeFalsy();
-        expect($scope.filterBySearchedAsset({ "name": "MyAbcExample.png", "type": "img" })).toBeFalsy();
       });
     });
-  });
 
+
+    describe('Widget editor', function(){
+
+      beforeEach(inject(function($injector) {
+        $injector.get('$controller')('AssetCtrl', {
+          $scope: $scope,
+          artifact: {
+            assets : [
+              {name: 'myAsset'}
+            ]
+          },
+          artifactRepo: {},
+          mode : 'widget',
+          assets: assets
+        });
+      }));
+
+      it('should put assets in $scope', function(){
+        expect($scope.assets).toEqual([{name: 'myAsset'}]);
+      });
+
+      it('should return isAssetPage=false when artifact is a widget', function(){
+        expect($scope.isAssetPage).toBeFalsy();
+      });
+    });
+
+  });
 })();
