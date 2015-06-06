06bbfbc1a0812d19c72c8e5ce916bf2d293f45e9
Vincent <vincent.elcrin@gmail.com>
2015-06-06 15:54:46 +0200

    Refactor code

--- backend/webapp/src/main/generator/js/directives/pbPropertyValues.js
+++ backend/webapp/src/main/generator/js/directives/pbPropertyValues.js
@@ -2,36 +2,16 @@
   'use strict';
 
   angular.module('pb.generator.directives')
-    .directive('pbPropertyValues', function ($parse, bindingsFactory, propertyValuesFactory) {
+    .directive('pbPropertyValues', function ($parse, bindingsFactory, propertyValuesFactory, bindingContextFactory) {
 
       function pbPropertyValuesCompile(tElement, tAttributes) {
         return {
           pre: function ($scope, elem, attr, pbModelCtrl) {
-            var context = pbModelCtrl.createGateway();
-            Object.defineProperty(context, '$item', {
-              get: function () {
-                return $scope.$item;
-              },
-              set: function (value) {
-                if ($scope.$collection) {
-                  $scope.$collection[$scope.$index] = value;
-                }
-              },
-              enumerable: true
-            });
-
-            Object.defineProperty(context, '$form', {
-              get: function () {
-                return $scope.$form;
-              },
-              set: function (value) {
-                $scope.$form = value;
-              },
-              enumerable: true
-            });
-
             var pbPropertyValues = propertyValuesFactory.get(tAttributes.pbPropertyValues);
-            bindingsFactory.create(pbPropertyValues, context, $scope.properties = {});
+            bindingsFactory.create(
+              pbPropertyValues,
+              bindingContextFactory.create(pbModelCtrl, $scope),
+              $scope.properties = {});
             $scope.properties.isBound = function (property) {
               return !!(pbPropertyValues[property] && pbPropertyValues[property].type === 'data');
             }
