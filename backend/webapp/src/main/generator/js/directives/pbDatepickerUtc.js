ae5aa27d8572085aaede6da9a4bded613c600da4
Lionel Breduillieard <lionel@lumographe.fr>
2015-06-18 16:03:06 +0200

    convert int value to date

--- backend/webapp/src/main/generator/js/directives/pbDatepickerUtc.js
+++ backend/webapp/src/main/generator/js/directives/pbDatepickerUtc.js
@@ -4,19 +4,33 @@
   angular.module('pb.generator.directives')
     .directive('pbDatepickerUtc', function ($parse, $filter) {
       function datepickerUtcLink(scope, element, attr, ngModelController) {
-
           // called with a JavaScript Date object when picked from the datepicker
           ngModelController.$parsers.push(function (viewValue) {
             if (angular.isDate(viewValue)) {
+              // fix utc
               viewValue.setMinutes(viewValue.getMinutes() - viewValue.getTimezoneOffset());
+              // reset hours, min, sec...
+              viewValue.setUTCHours(0,0,0,0);
             }
             return viewValue;
           });
 
           // called with a 'yyyy-mm-dd' string to format
           ngModelController.$formatters.push(function (modelValue) {
+            if (needDateConversion(modelValue)) {
+              var date = new Date(modelValue);
+              date.setUTCHours(0,0,0,0);
+              ngModelController.$setViewValue(date);
+              return $filter('date')( date , attr.datepickerPopup);
+            }
             return $filter('date')( modelValue , attr.datepickerPopup);
           });
+
+      }
+
+      function needDateConversion(value) {
+        var val = Number(value);
+        return Math.floor(val) === val;
       }
 
       return  {
