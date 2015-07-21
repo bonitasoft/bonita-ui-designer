faa89af01d9d0730a0c9709b3a48769a6c3c4d9b
Vincent <vincent.elcrin@gmail.com>
2015-07-21 15:45:15 +0200

    Add container visitor to collect any kind of containers within a page

---
+++ backend/webapp/src/test/java/org/bonitasoft/web/designer/visitors/AnyContainerVisitorTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2015 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.web.designer.visitors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.bonitasoft.web.designer.builder.ComponentBuilder.aComponent;
+import static org.bonitasoft.web.designer.builder.ContainerBuilder.aContainer;
+import static org.bonitasoft.web.designer.builder.FormContainerBuilder.aFormContainer;
+import static org.bonitasoft.web.designer.builder.PageBuilder.aPage;
+import static org.bonitasoft.web.designer.builder.TabBuilder.aTab;
+import static org.bonitasoft.web.designer.builder.TabsContainerBuilder.aTabsContainer;
+
+import org.bonitasoft.web.designer.model.page.Component;
+import org.bonitasoft.web.designer.model.page.Container;
+import org.bonitasoft.web.designer.model.page.FormContainer;
+import org.bonitasoft.web.designer.model.page.TabsContainer;
+import org.bonitasoft.web.designer.visitor.AnyContainerVisitor;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AnyContainerVisitorTest {
+
+    @InjectMocks
+    private AnyContainerVisitor anyContainerVisitor;
+
+    private Component component;
+
+    @Before
+    public void setUp() throws Exception {
+        component = aComponent().build();
+    }
+
+    @Test
+    public void should_not_collect_components() throws Exception {
+        assertThat(anyContainerVisitor.visit(component)).isEmpty();
+    }
+
+    @Test
+    public void should_collect_containers() throws Exception {
+        Container container = aContainer()
+                .with(component)
+                .build();
+
+        assertThat(anyContainerVisitor.visit(container)).containsExactly(container);
+    }
+
+    @Test
+    public void should_collect_tabs_containers_and_content() throws Exception {
+        Container container = aContainer().build();
+        TabsContainer tabsContainer = aTabsContainer()
+                .with(aTab()
+                        .with(container))
+                .build();
+
+        assertThat(anyContainerVisitor.visit(tabsContainer)).containsExactly(tabsContainer, container);
+    }
+
+    @Test
+    public void should_collect_form_containers_and_content() throws Exception {
+        Container container = aContainer().build();
+        FormContainer formContainer = aFormContainer().with(container).build();
+
+        assertThat(anyContainerVisitor.visit(formContainer)).containsExactly(formContainer, container);
+    }
+
+    @Test
+    public void should_collect_container_within_previewable() throws Exception {
+        Container container = aContainer().build();
+
+        assertThat(anyContainerVisitor.visit(aPage()
+                .with(container)
+                .build())).containsExactly(container);
+    }
+}
