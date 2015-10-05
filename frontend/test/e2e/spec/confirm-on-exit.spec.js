6e086f6521f0c8b37ce6fbf2754c3ece47bfe38b
Vincent <vincent.elcrin@gmail.com>
2015-10-05 14:56:58 +0200

    Bump protractor version up to 2.4.0

--- frontend/test/e2e/spec/confirm-on-exit.spec.js
+++ frontend/test/e2e/spec/confirm-on-exit.spec.js
@@ -16,7 +16,7 @@ describe('confirm on exit', function() {
     editor.addWidget('pbParagraph');
     editor.back();
 
-    var dialog = browser.switchTo().alert();
+    var dialog = switchToAlert();
     expect(dialog.accept).toBeDefined();
     dialog.accept();
     expect(browser.getCurrentUrl()).toMatch(/\/home/);
@@ -26,9 +26,19 @@ describe('confirm on exit', function() {
     editor.addWidget('pbParagraph');
     editor.back();
 
-    var dialog = browser.switchTo().alert();
+    var dialog = switchToAlert();
     expect(dialog.dismiss).toBeDefined();
     dialog.dismiss();
     expect(browser.getCurrentUrl()).toMatch(/\/empty/);
   });
+
+  /**
+   * Based on
+   * https://github.com/angular/protractor/issues/1486
+   * https://github.com/angular/angular.js/commit/addb1ae37d775937688ae6f09e6f0ebd79225849
+   */
+  function switchToAlert() {
+    browser.wait(protractor.ExpectedConditions.alertIsPresent(), 1000);
+    return browser.switchTo().alert();
+  }
 });
