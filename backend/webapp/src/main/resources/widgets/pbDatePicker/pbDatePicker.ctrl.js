b17acf2427bf2a1ae006e051c7acb2e12d4fb8a4
Colin PUY <colin.puy@bonitasoft.com>
2015-11-02 16:05:30 +0100

    New date picker implementation based on angular strap

--- backend/webapp/src/main/resources/widgets/pbDatePicker/pbDatePicker.ctrl.js
+++ backend/webapp/src/main/resources/widgets/pbDatePicker/pbDatePicker.ctrl.js
@@ -1,34 +1,12 @@
-function PbDatePickerCtrl($scope, $log, widgetNameFactory) {
+function PbDatePickerCtrl($scope, $log, widgetNameFactory, $element) {
 
   'use strict';
 
-  //By default the picker is closed
-  this.opened = false;
-
-  this.datepickerOptions = {
-    startingDay: 1
-  };
-
-  this.today = function() {
-    $scope.properties.value = new Date();
-    this.floorDate();
-  };
-
-  this.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    if (!$scope.properties.readOnly) {
-      this.opened = true;
-    }
-  };
-
-  this.floorDate = function() {
-    if (angular.isDate($scope.properties.value)) {
-      $scope.properties.value.setTime(Date.UTC($scope.properties.value.getFullYear(), $scope.properties.value.getMonth(), $scope.properties.value.getDate(), 0, 0, 0, 0));
-    }
-  };
-
   this.name = widgetNameFactory.getName('pbDatepicker');
+
+  this.open = function () {
+    angular.element($element).find('input').triggerHandler('click');
+  };
 
   if (!$scope.properties.isBound('value')) {
     $log.error('the pbDatepicker property named "value" need to be bound to a variable');
