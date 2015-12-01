943a44e2f7c707996f9bfcc4c859a5e71f31352d
julien-reboul <julien.reboul@bonitasoft.com>
2015-12-01 10:46:46 +0100

    [pbDatePicker] fix test that choose current 21st day of month

--- tests/src/test/spec/pages/datePicker.spec.js
+++ tests/src/test/spec/pages/datePicker.spec.js
@@ -6,7 +6,7 @@ describe('date picker', function () {
     $('input[name="pbDatepicker0"]').click();
     element(by.cssContainingText('.dropdown-menu table td button', '21')).click();
     expect($('pb-text').getText()).toMatch(/"\d\d\d\d-\d\d-21T00:00:00.000Z"/);
-    expect($('input[name="pbDatepicker1"]').getAttribute('value')).toBe('2015/11/21');
+    expect($('input[name="pbDatepicker1"]').getAttribute('value')).toMatch(/\d\d\d\d\/\d\d\/21/);
 
     var value = $('input[name="pbDatepicker2"]').getAttribute('value');
     expect(value).toBe('11/05/2015');
