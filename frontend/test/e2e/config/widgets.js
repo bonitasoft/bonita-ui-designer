3c11a05465bf338953c73a5f65d3e7cfa24c9607
Bonita CI <bonita-ci@bonitasoft.com>
2016-03-23 10:43:33 +0000

    [RELEASE] Create release 1.2.17

--- frontend/test/e2e/config/widgets.js
+++ frontend/test/e2e/config/widgets.js
@@ -6,7 +6,7 @@
     .module('bonitasoft.designer.e2e')
     .value('widgets', [
       {
-        designerVersion: '1.2.9-SNAPSHOT',
+        designerVersion: '1.2.17',
         id: 'pbParagraph',
         name: 'Paragraph',
         lastUpdate: 1430212276146,
@@ -31,7 +31,7 @@
         }]
       },
       {
-        designerVersion: '1.2.9-SNAPSHOT',
+        designerVersion: '1.2.17',
         id: 'pbInput',
         name: 'Input',
         lastUpdate: 1430212276119,
