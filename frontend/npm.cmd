751c564954b33b3805974db45e2f2a7949c7f253
Colin PUY <colin.puy@bonitasoft.com>
2016-07-11 10:44:06 +0200

    tr(mvn): bump maven-frontend-plugin version to 1.0 (#1977)
    Also update node and npm versions.
    node => v4.4.7
    npm => 2.15.8

--- frontend/npm.cmd
+++
@@ -1,6 +0,0 @@
-REM This file is needed by npm-frontend-plugin since due to a bug, this plugin doesn't work whithout local node installation for our need
-REM https://github.com/eirslett/frontend-maven-plugin/issues/204
-
-@echo off
-%~dp0node/npm %*
-@echo on
