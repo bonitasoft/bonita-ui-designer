eedfaff3d77978a2391c513057ac8a449e45f7c0
bonita-ci <bonita-ci@bonitasoft.com>
2016-08-26 08:38:52 +0200

    release(1.4.5) Create release 1.4.5

--- frontend/test/e2e/config/widgets.js
+++ frontend/test/e2e/config/widgets.js
@@ -6,7 +6,7 @@
     .module('bonitasoft.designer.e2e')
     .value('widgets', [
       {
-        designerVersion: '1.4-SNAPSHOT',
+        designerVersion: '1.4.5',
         id: 'pbParagraph',
         name: 'Paragraph',
         lastUpdate: 1430212276146,
@@ -31,7 +31,7 @@
         }]
       },
       {
-        designerVersion: '1.4-SNAPSHOT',
+        designerVersion: '1.4.5',
         id: 'pbInput',
         name: 'Input',
         lastUpdate: 1430212276119,
@@ -224,7 +224,7 @@
         ],
         assets: []
       }, {
-        'designerVersion': '1.4-SNAPSHOT',
+        'designerVersion': '1.4.5',
         'favorite': false,
         'id': 'pbTitle',
         'name': 'Title',
