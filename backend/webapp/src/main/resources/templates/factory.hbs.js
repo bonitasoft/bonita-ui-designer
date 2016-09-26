dd8d7c2ded985fc74f6040b6699129ff9a649413
Colin PUY <colin.puy@bonitasoft.com>
2016-09-26 14:13:56 +0200

    tr(handlebar) bump handlebar version to 4.0.6 (#2057)
    - Bump handlerbar version to 4.0.6
    - Fix templates according to new version
    - Remove unused template
    - Fix irrelevant test

--- backend/webapp/src/main/resources/templates/factory.hbs.js
+++ backend/webapp/src/main/resources/templates/factory.hbs.js
@@ -1,7 +1,7 @@
 angular.module('bonitasoft.ui.services').factory('{{name}}Factory', function() {
   var resources = {};
   {{#each resources}}
-  resources['{{ key }}'] =  {{ json this }};
+  resources['{{ @key }}'] =  {{ json this }};
   {{/each}}
   return {
     get: function(uuid) {
