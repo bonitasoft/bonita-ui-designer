29b5e1b0dccaa5a7fc41f9d428c6894b338235db
abirembaut <anthony.birembaut@gmail.com>
2016-11-02 16:42:15 +0100

    feat(link widget) Support link to process or task form (#2082)
    - Update the link widget to offer a choice between custom URL and
    prebuild ones (Application page, process form, task form, case overview)
    - Support the app parameter to forward the Application name to the
    process/task forms when it is present
    
    Covers [BS-15954](https://bonitasoft.atlassian.net/browse/BS-15954)

--- backend/webapp/src/test/javascript/spec/widgets/pbLink.spec.js
+++ backend/webapp/src/test/javascript/spec/widgets/pbLink.spec.js
@@ -1,13 +1,18 @@
 describe('pbLink', function() {
 
-  var compile, scope, dom;
+  var compile, scope, dom, location;
 
   beforeEach(module('bonitasoft.ui.widgets'));
 
   beforeEach(inject(function ($injector){
     compile = $injector.get('$compile');
     scope = $injector.get('$rootScope').$new();
+    location = $injector.get('$location');
+    location.absUrl = function() {
+      return 'http://localhost:8080/bonita/portal/resource/app/myApp/tasks/content/?app=myApp'
+    };
     scope.properties = {
+      type: 'URL',
       alignment: 'left',
       buttonStyle: 'none'
     };
@@ -20,7 +25,7 @@ describe('pbLink', function() {
 
   it('should contain a a inside a div', function() {
     // Ivre, il ne connait pas div a ou div > a... WTF
-    expect(dom.find('div').find('a')[0]).toBeDefined();
+    expect(dom.find('a')[0]).toBeDefined();
   });
 
   it('should position the div to the left', function() {
@@ -28,40 +33,89 @@ describe('pbLink', function() {
   });
 
   it('should not have a btn class if no buttonStyle is defined', function() {
-    expect(dom.find('div').find('a').hasClass('btn')).toBe(false);
+    expect(dom.find('a').hasClass('btn')).toBe(false);
   });
 
   it('should set a btn class for the button style chosen', function() {
     scope.properties.buttonStyle = 'danger';
     scope.$apply();
-    expect(dom.find('div').find('a').hasClass('btn')).toBe(true);
-    expect(dom.find('div').find('a').hasClass('btn-danger')).toBe(true);
+    expect(dom.find('a').hasClass('btn')).toBe(true);
+    expect(dom.find('a').hasClass('btn-danger')).toBe(true);
 
     scope.properties.buttonStyle = 'info';
     scope.$apply();
-    expect(dom.find('div').find('a').hasClass('btn-info')).toBe(true);
+    expect(dom.find('a').hasClass('btn-info')).toBe(true);
   });
 
-  it('should set an url', function() {
-    expect(dom.find('div').find('a').attr('href')).toBe(void 0);
+  it('should set an url when type is custom URL', function() {
+    expect(dom.find('a').attr('href')).toBeUndefined();
     scope.properties.targetUrl = 'http://google.fr';
     scope.$apply();
-    expect(dom.find('div').find('a').attr('href')).toBe('http://google.fr');
+    expect(dom.find('a').attr('href')).toBe('http://google.fr');
   });
 
   it('should set a target', function() {
-    expect(dom.find('div').find('a').attr('target')).toBe('');
+    expect(dom.find('a').attr('target')).toBe('');
     scope.properties.target = '_blank';
     scope.$apply();
-    expect(dom.find('div').find('a').attr('target')).toBe('_blank');
+    expect(dom.find('a').attr('target')).toBe('_blank');
   });
 
   it('should set a text', function() {
-    expect(dom.find('div').find('a').text()).toBe('');
+    expect(dom.find('a').text()).toBe('');
     scope.properties.text = '<i class="fa fa-bonita">yolo</i>';
     scope.$apply();
-    expect(dom.find('div').find('a').text()).toBe('yolo');
-    expect(dom.find('div').find('a').html()).toBe('<i class="fa fa-bonita">yolo</i>');
+    expect(dom.find('a').text()).toBe('yolo');
+    expect(dom.find('a').html()).toBe('<i class="fa fa-bonita">yolo</i>');
+  });
+
+  it('should set target to top when type is page', function() {
+    expect(dom.find('a').attr('target')).toBe('');
+    scope.properties.type = 'page';
+    scope.$apply();
+    expect(dom.find('a').attr('target')).toBe('_top');
+  });
+
+  it('should set URL when type is page', function() {
+    expect(dom.find('a').attr('href')).toBeUndefined();
+    scope.properties.type = 'page';
+    scope.properties.pageToken = 'mySecondPage';
+    scope.$apply();
+    expect(dom.find('a').attr('href')).toContain('../mySecondPage');
+  });
+
+  it('should set URL when type is start process', function() {
+    expect(dom.find('a').attr('href')).toBeUndefined();
+    scope.properties.type = 'process';
+    scope.properties.processName = 'Pool';
+    scope.properties.processVersion = '1.0';
+    scope.$apply();
+    expect(dom.find('a').attr('href')).toBe('http://localhost:8080/bonita/portal/form/process/Pool/1.0?app=myApp');
+  });
+
+  it('should set URL when type is perform task', function() {
+    expect(dom.find('div').find('a').attr('href')).toBeUndefined();
+    scope.properties.type = 'task';
+    scope.properties.taskId = '42';
+    scope.$apply();
+    expect(dom.find('div').find('a').attr('href')).toBe('http://localhost:8080/bonita/portal/form/taskInstance/42?app=myApp');
+  });
+
+  it('should set URL when type is case overview', function() {
+    expect(dom.find('a').attr('href')).toBeUndefined();
+    scope.properties.type = 'overview';
+    scope.properties.caseId = '12';
+    scope.$apply();
+    expect(dom.find('a').attr('href')).toBe('http://localhost:8080/bonita/portal/form/processInstance/12?app=myApp');
+  });
+
+  it('should set App token in URL when the property is set', function() {
+    expect(dom.find('div').find('a').attr('href')).toBeUndefined();
+    scope.properties.type = 'task';
+    scope.properties.taskId = '42';
+    scope.properties.appToken = 'livingApp';
+    scope.$apply();
+    expect(dom.find('div').find('a').attr('href')).toBe('http://localhost:8080/bonita/portal/form/taskInstance/42?app=livingApp');
   });
 
 });
