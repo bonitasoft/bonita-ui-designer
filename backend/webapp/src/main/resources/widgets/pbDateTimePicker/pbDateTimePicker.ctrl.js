78a7876cead07baff1cfc71d2610c27edc9eed84
benjaminParisel <benjamin.parisel@bonitasoft.com>
2017-03-30 13:25:31 +0200

    fix(dateTimePicker): improve visual of dateTimePickerWidget (#2227)
    remove white space into input date and input time
    add today button and his propreties associate
    update and add test
    
    Closes [BS-16176](https://bonitasoft.atlassian.net/browse/BS-16176)

--- backend/webapp/src/main/resources/widgets/pbDateTimePicker/pbDateTimePicker.ctrl.js
+++ backend/webapp/src/main/resources/widgets/pbDateTimePicker/pbDateTimePicker.ctrl.js
@@ -8,27 +8,41 @@ function PbDateTimePickerCtrl($scope, $log, widgetNameFactory, $element, $locale
   $bsDatepicker.defaults.keyboard = false;
 
   this.setDateAndTimeToNow = function() {
+    var now = new moment();
+    now.minute(Math.round(now.minute() / 5) * 5).second(0);
+    this.formatMoment(now);
+  };
+
+  this.setDateToToday = function() {
+    var now;
+    if ($scope.properties.value) {
+      // Set the date at today but don't change time
+      var input = new moment($scope.properties.value);
+      now = new moment({hour: input.hours(), minute: input.minutes(), seconds: input.seconds()});
+    } else {
+      now = new moment({hour: 0, minute: 0, seconds: 0})
+    }
+    this.formatMoment(now);
+  };
+
+  this.formatMoment = function(moment) {
     var isoFormat = 'YYYY-MM-DDTHH:mm:ss';
     if ($scope.properties.withTimeZone) {
-      var now = new moment().utc();
-      $scope.properties.value = now.format(isoFormat) + 'Z';
+      $scope.properties.value = moment.utc().format(isoFormat) + 'Z';
     } else {
-      var now = new moment();
-      $scope.properties.value = now.format(isoFormat);
+      $scope.properties.value = moment.format(isoFormat);
     }
   };
 
-  this.openDatePicker = function () {
+  this.openDatePicker = function() {
     $element.find('input')[0].focus();
   };
 
-  this.openTimePicker = function () {
+  this.openTimePicker = function() {
     $element.find('input')[1].focus();
   };
 
   if (!$scope.properties.isBound('value')) {
     $log.error('the pbDateTimepicker property named "value" need to be bound to a variable');
   }
-
-
 }
---
+++ tests/src/test/spec/pages/dateTimePicker.spec.js
@@ -0,0 +1,21 @@
+describe('date time picker', function () {
+
+  it('should display dates and time not taking timezone in account', function () {
+    browser.get('/designer/preview/page/dateTimePicker/');
+
+    $('input[name="pbDateTimepicker0date"]').click();
+    element(by.cssContainingText('.dropdown-menu table td button', '21')).click();
+    expect($('input[name="pbDateTimepicker0date"]').getAttribute('value')).toMatch(/\d\d\/21\/\d\d\d\d/);
+    expect($('input[name="pbDateTimepicker0time"]').getAttribute('value')).toMatch("4:17:00 PM");
+
+   });
+
+  it('should show error date message on wrong date', function () {
+    browser.get('/designer/preview/page/dateTimePicker/');
+
+    $('input[name="pbDateTimepicker0date"]').click();
+    $('input[name="pbDateTimepicker0date"]').sendKeys('bonita');
+
+    expect($('.text-danger').getText()).toEqual('This is not a valid date');
+  });
+});
