09ac691ee87841938bc49e84c36e0206cb727e7e
anthony <anthony.birembaut@bonitasoft.com>
2017-04-03 15:13:37 +0200

    fix(date filter): provide date filter
    - make uiDate filter tests independent from browser timezone
    
    Covers [BS-16399](https://bonitasoft.atlassian.net/browse/BS-16399)

--- backend/webapp/src/test/javascript/spec/filters/uiDate.filter.spec.js
+++ backend/webapp/src/test/javascript/spec/filters/uiDate.filter.spec.js
@@ -2,11 +2,12 @@ describe('uiDate filter', function () {
 
   beforeEach(module('bonitasoft.ui.filters'));
 
-  var uiDateFilter;
+  var uiDateFilter, $filter;
 
-  beforeEach(inject(function (_uiDateFilter_) {
+  beforeEach(inject(function (_uiDateFilter_, _$filter_) {
 
     uiDateFilter = _uiDateFilter_;
+    $filter = _$filter_;
 
   }));
 
@@ -15,20 +16,28 @@ describe('uiDate filter', function () {
   });
 
   it('should convert long to UTC date', function () {
-    expect(uiDateFilter(1490918400000, 'medium')).toBe('Mar 31, 2017 12:00:00 AM');
+    expect(uiDateFilter(1490918400000, 'yyyy/MM/dd HH:mm:ss')).toBe('2017/03/31 00:00:00');
   });
 
   it('should convert ISO string to date', function () {
     expect(uiDateFilter('2017-03-31')).toBe('Mar 31, 2017');
   });
 
+  it('should convert ISO string to date with format', function () {
+    expect(uiDateFilter('2017-03-31', 'yyyy/MM/dd')).toBe('2017/03/31');
+  });
+
   it('should convert ISO string to date and time', function () {
     expect(uiDateFilter('2017-03-31T00:00:00')).toBe('Mar 31, 2017 12:00:00 AM');
   });
 
+  it('should convert ISO string to date and time with format', function () {
+    expect(uiDateFilter('2017-03-31T00:00:00', 'yyyy/MM/dd HH:mm:ss')).toBe('2017/03/31 00:00:00');
+  });
+
   it('should convert ISO string with timezone to date and time', function () {
-    expect(uiDateFilter('2017-03-31T00:00:00Z')).toContain('Mar 31, 2017');
-    expect(uiDateFilter('2017-03-31T00:00:00Z')).not.toBe('Mar 31, 2017');
+    var isoDateWithLocalTimezone = uiDateFilter('2017-03-31T00:00:00Z', 'yyyy-MM-ddTHH:mm:ssZ');
+    expect($filter('date')(isoDateWithLocalTimezone, 'yyyy-MM-ddTHH:mm:ss', 'UTC')).toBe('2017-03-31T00:00:00');
   });
 
 });
