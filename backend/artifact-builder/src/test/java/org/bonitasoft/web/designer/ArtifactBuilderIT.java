0b197c3324c19493e8c24fa143dbd6d01378ed9f
julienmege <julien.mege@bonitasoft.com>
2021-07-05 11:14:15 +0200

    defect(export): Add IT tests for export fragment contained in pages/f… (#87)
    * defect(export): Add IT tests for export fragment contained in pages/fragments
    
    Covers [UID-469](https://bonitasoft.atlassian.net/browse/UID-469)
    
    * factorize artifacts creation

--- backend/artifact-builder/src/test/java/org/bonitasoft/web/designer/ArtifactBuilderIT.java
+++ backend/artifact-builder/src/test/java/org/bonitasoft/web/designer/ArtifactBuilderIT.java
@@ -5,7 +5,17 @@ import org.bonitasoft.web.designer.config.UiDesignerPropertiesBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.commons.io.FileUtils.*;
@@ -27,6 +37,7 @@ public class ArtifactBuilderIT {
         await().atMost(1, SECONDS).until(() -> {
             try {
                 deleteDirectory(properties.getWorkspace().getPages().getDir().toFile());
+                deleteDirectory(properties.getWorkspace().getFragments().getDir().toFile());
                 return true;
             } catch (Exception e) {
                 return false;
@@ -39,25 +50,46 @@ public class ArtifactBuilderIT {
 
         // Given
         var pageId = "ma-page";
-        var target = properties.getWorkspace().getPages().getDir().resolve(pageId);
-        var source = Path.of("./src/test/resources/workspace/pages/" + pageId);
-        copyDirectory(source.toFile(), target.toFile());
+        createPage(pageId);
+        createFragment("person");
+        createFragment("details");
 
         // When
         artifactBuilder = new ArtifactBuilderFactory(properties).create();
         var page = artifactBuilder.buildPage(pageId);
+        List<String> zipEntriesName = extractZipEntriesName(page);
 
         // Then
         assertThat(page).isNotEmpty();
+        assertThat(zipEntriesName).contains("resources/page.json");
+        assertThat(zipEntriesName).contains("resources/fragments/person/person.json");
+        assertThat(zipEntriesName).contains("resources/fragments/details/details.json");
+    }
+
+    @Test
+    public void should_export_fragment() throws Exception {
+
+        // Given
+        var fragmentPerson = "person";
+        createFragment(fragmentPerson);
+        createFragment("details");
+
+        // When
+        artifactBuilder = new ArtifactBuilderFactory(properties).create();
+        var page = artifactBuilder.buildFragment(fragmentPerson);
+        List<String> zipEntriesName = extractZipEntriesName(page);
+
+        // Then
+        assertThat(page).isNotEmpty();
+        assertThat(zipEntriesName).contains("resources/fragment.json");
+        assertThat(zipEntriesName).contains("resources/fragments/details/details.json");
     }
 
     @Test
     public void should_index_pages() throws Exception {
         // Given
-        var pageId = "ma-page";
-        var target = properties.getWorkspace().getPages().getDir().resolve(pageId);
-        var source = Path.of("./src/test/resources/workspace/pages/" + pageId);
-        copyDirectory(source.toFile(), target.toFile());
+        createPage("ma-page");
+
         // When
         artifactBuilder = new ArtifactBuilderFactory(properties).create();
 
@@ -73,9 +105,9 @@ public class ArtifactBuilderIT {
         var pageId = "ma-page";
         var target = properties.getWorkspace().getPages().getDir().resolve(pageId);
         var source = Path.of("./src/test/resources/workspace/pages/" + pageId);
-        copyDirectory(source.toFile(), target.toFile());
-        artifactBuilder = new ArtifactBuilderFactory(properties).create();
+        createPage(pageId);
 
+        artifactBuilder = new ArtifactBuilderFactory(properties).create();
         var jsonIndex = properties.getWorkspace().getPages().getDir().resolve(".metadata/.index.json");
 
         // Delete page folder and index metadata (index should be recreated on the fly when copying back the page folder thanks to watcher)
@@ -89,5 +121,36 @@ public class ArtifactBuilderIT {
         await().atMost(1, SECONDS).untilAsserted(() ->
                 assertThat(jsonIndex).exists()
         );
+    }
+
+    private void createPage(String pageId) throws IOException {
+        var target = properties.getWorkspace().getPages().getDir().resolve(pageId);
+        var source = Path.of("./src/test/resources/workspace/pages/" + pageId);
+        copyDirectory(source.toFile(), target.toFile());
+    }
+
+    private void createFragment(String fragmentId) throws IOException {
+        var target = properties.getWorkspace().getFragments().getDir().resolve(fragmentId);
+        var source = Path.of("./src/test/resources/workspace/fragments/" + fragmentId);
+        copyDirectory(source.toFile(), target.toFile());
+    }
+
+    private List<String> extractZipEntriesName(byte[] content) throws IOException {
+        List<String> entries = new ArrayList<>();
+
+        ZipInputStream zi = null;
+        try {
+            zi = new ZipInputStream(new ByteArrayInputStream(content));
+
+            ZipEntry zipEntry = null;
+            while ((zipEntry = zi.getNextEntry()) != null) {
+                entries.add(zipEntry.getName());
+            }
+        } finally {
+            if (zi != null) {
+                zi.close();
+            }
+        }
+        return entries;
     }
 }
