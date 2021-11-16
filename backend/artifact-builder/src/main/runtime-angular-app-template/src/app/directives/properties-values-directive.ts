388ede3995e9209c148c6a81e37b666e28ea8a99
Benjamin Parisel <benjamin.parisel@bonitasoft.com>
2021-11-16 16:31:31 +0100

    feat(repeatedCollection): Container is now fully functional (#123)
    * update component and container template
    * $item, $count, $collection and $index is now usable in propertiesValues
    
    Covers [UID-564](https://bonitasoft.atlassian.net/browse/UID-564)

--- backend/artifact-builder/src/main/runtime-angular-app-template/src/app/directives/properties-values-directive.ts
+++ backend/artifact-builder/src/main/runtime-angular-app-template/src/app/directives/properties-values-directive.ts
@@ -1,7 +1,7 @@
 import { Directive, Input, OnInit, ViewContainerRef, TemplateRef } from '@angular/core';
 import propertiesValues from '../../assets/propertiesValues';
 import { uidModel } from './uid-model-directive';
-import { BindingFactory, EnumBinding, VariableContext } from '@bonitasoft/ui-designer-context-binding';
+import { BindingContextFactory, BindingFactory, EnumBinding, VariableContext } from '@bonitasoft/ui-designer-context-binding';
 
 /**
  * Allow us to declare a directive to setup a structural directive like following
@@ -12,24 +12,46 @@ import { BindingFactory, EnumBinding, VariableContext } from '@bonitasoft/ui-des
     selector: '[properties-values]'
 })
 export class PropertiesValues implements OnInit {
+    private collection: Array<any> | undefined;
     @Input('properties-values') reference: string = '';
 
-    // Properties input
-    @Input('properties') properties: any = {};
+    @Input('properties-valuesCollection') set setCollection(collection: Array<any>) {
+        this.scope.collection = collection;
+    }
 
+    @Input('properties-valuesIndex') set setIndex(index: number) {
+        this.scope.index = index;
+    }
+
+    @Input('properties-valuesItem') set setItem(item: any) {
+        this.scope.item = item;
+    }
+
+    @Input('properties-valuesCount') set setCount(count: number) {
+        this.scope.count = count;
+    }
+
+    private properties: any = {};
     private context: VariableContext = {};
     private model: uidModel;
+    private scope: any = {};
+
+    private bindingContextFactory: BindingContextFactory;
 
     constructor(private templateRef: TemplateRef<any>,
-        private viewContainerRef: ViewContainerRef, private uidModel: uidModel) {
+                private viewContainerRef: ViewContainerRef, private uidModel: uidModel) {
         this.model = uidModel;
+        this.bindingContextFactory = new BindingContextFactory();
     }
-    ngOnInit(): void {
+
+    async ngOnInit(): Promise<void> {
         const propertyValues = propertiesValues.get(this.reference);
+
+        let variablesAccessors = new Map(this.model.getVariableAccessors());
 
         BindingFactory.createPropertiesBinding(
             propertyValues,
-            this.model.getVariableAccessors(),
+            this.bindingContextFactory.addSpecificKeywordOnAccessors(variablesAccessors, this.scope),
             this.properties);
 
         this.properties.isBound = function (propertyName: string) {
