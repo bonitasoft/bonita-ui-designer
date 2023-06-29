0a2dda03edb596ea931fd042ac44e5e142eaef4a
Benjamin Parisel <benjamin.parisel@bonitasoft.com>
2023-06-29 12:05:09 +0200

    feat(widget): debounce property for input (#453)
    * Add a debounce property for pbInput and pbAutocomplete
    
    (inspired from commit 628f457827457483c76d02a758dcd21643bca23d)

--- backend/artifact-builder/src/test/javascript/spec/widgets/pbInput.spec.js
+++ backend/artifact-builder/src/test/javascript/spec/widgets/pbInput.spec.js
@@ -1,13 +1,14 @@
 describe('pbInput', function () {
 
-  var $compile, scope;
+  var $compile, scope, $timeout;
 
  beforeEach(module('bonitasoft.ui.services'));
   beforeEach(module('bonitasoft.ui.widgets'));
 
-  beforeEach(inject(function (_$compile_, $rootScope) {
+  beforeEach(inject(function (_$compile_, $rootScope, $injector) {
     $compile = _$compile_;
     scope = $rootScope.$new();
+    $timeout = $injector.get('$timeout');
     scope.properties = {
       isBound: function() {
         return false;
@@ -175,9 +176,9 @@ describe('pbInput', function () {
       scope.properties.type = 'number';
       scope.properties.step = 0.5;
       var element = $compile('<pb-input></pb-input>')(scope);
-      
+
       /*
-       * This part of the test would simulate the step feature 
+       * This part of the test would simulate the step feature
        * of the input using keyup but the triggerHandler do not
        * send the correct event and the value is not updated
        *
@@ -190,6 +191,23 @@ describe('pbInput', function () {
       expect(element.find('input').attr('step')).toEqual('0.5');
 
 //      expect(scope.properties.value).toEqual(1);
+    });
+
+    it('should set a debounce value on input', function () {
+      scope.properties.debounce = 200;
+      scope.properties.value = '';
+
+      const element = $compile('<pb-input></pb-input>')(scope);
+      scope.$apply();
+
+      element.find('input').val('barfoo');
+      element.find('input').triggerHandler('input');
+
+      expect(scope.properties.value).toBe('');
+
+      $timeout.flush();
+
+      expect(scope.properties.value).toBe('barfoo');
     });
 
   });
